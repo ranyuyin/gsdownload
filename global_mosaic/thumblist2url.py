a8be06ad03e96edb77dd290002675048382bbf7a
yinranyu <187345994@qq.com>
2019-10-04 23:38:54 +0800

    update

--- global_mosaic/thumblist2url.py
+++ global_mosaic/thumblist2url.py
@@ -9,10 +9,14 @@ if __name__ == '__main__':
     parser.add_argument('-t', help='path google cloud index.csv', dest='gstable',
                         required=False, default=r'Z:\yinry\Landsat.Data\GOOGLE\landsat_index.csv.gz')
     parser.add_argument('-o', help='output url list file', dest='ourllist', required=False, default='')
+    parser.add_argument('-f', help='list file', dest='listfile', action='store_true')
     args = parser.parse_args()
-    jpglist = glob(path.join(args.thmdir, '*.jpg'))
+    if args.listfile:
+        jpglist = [path.basename(i.strip()) for i in open(args.thmdir).readlines()]
+    else:
+        jpglist = glob(path.join(args.thmdir, '*.jpg'))
+        jpglist = [path.basename(i) for i in jpglist]
     df, _ = down_util.split_collection(args.gstable)
-    jpglist = [path.basename(i) for i in jpglist]
     df = down_util.Get_zone(df, todoPID=jpglist)
     if args.ourllist is not '':
         ourlpath = args.ourllist
