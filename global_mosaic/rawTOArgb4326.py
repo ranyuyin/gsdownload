3d7649589ee03010a459312ba606907ff520a503
yinry <catfishy_0094>
2020-06-24 19:51:04 +0800

    updata

--- global_mosaic/rawTOArgb4326.py
+++ global_mosaic/rawTOArgb4326.py
@@ -376,16 +376,16 @@ def _main(args):
     keppTemp = args.keppTemp
     # scan List
     mtlList = glob(path.join(inFolder, '**', '*MTL.txt'), recursive=True)
-    for mtl in mtlList:
-        toMosaic(mtl,outFolder,maskCloud, OVERWRITE,pixel_sunangle,keppTemp)
-    # p = Pool(n_multi)
-    # try:
-    #     for i in tqdm(p.imap(partial(toMosaic, outFolder=outFolder, maskCloud=maskCloud, OVERWRITE=OVERWRITE,
-    #                                  pixel_sunangle=pixel_sunangle, keppTemp=keppTemp), mtlList), total=len(mtlList)):
-    #         pass
-    # except KeyboardInterrupt:
-    #     p.terminate()
-    #     p.join()
+    # for mtl in mtlList:
+    #     toMosaic(mtl,outFolder,maskCloud, OVERWRITE,pixel_sunangle,keppTemp)
+    p = Pool(n_multi)
+    try:
+        for i in tqdm(p.imap(partial(toMosaic, outFolder=outFolder, maskCloud=maskCloud, OVERWRITE=OVERWRITE,
+                                     pixel_sunangle=pixel_sunangle, keppTemp=keppTemp), mtlList), total=len(mtlList)):
+            pass
+    except KeyboardInterrupt:
+        p.terminate()
+        p.join()
 
 
 class LandsatDst:
@@ -487,7 +487,7 @@ class LandsatDst:
         self.maskcloud = maskCloud
         self.pixel_sunangle = pixel_sunangle
         self.toRGB()
-        cmdBase = 'gdalwarp -t_srs EPSG:4326 -co COMPRESSION=RLE -dstnodata 0 -overwrite' \
+        cmdBase = 'gdalwarp -t_srs EPSG:4326 -co COMPRESSION=RLE -dstnodata 0 -overwrite -q' \
                   ' -of PCIDSK -co TILESIZE=256 -co INTERLEAVING=TILED -r cubic -wm 3000 -srcnodata 0'
         cmdVar = ' -te {} {} {} {} -te_srs EPSG:4326 -tr {} {} {} {}'
         if self.cross180:
@@ -496,15 +496,15 @@ class LandsatDst:
             oP2 = getOname(self.pid, mosaicOfolder, '2')
             cmd1 = cmdBase + cmdVar.format(*te[0], self.oRes, self.oRes, self.tempRgbName, oP1)
             cmd2 = cmdBase + cmdVar.format(*te[1], self.oRes, self.oRes, self.tempRgbName, oP2)
-            print(cmd1)
-            print(cmd2)
+            # print(cmd1)
+            # print(cmd2)
             system(cmd1)
             system(cmd2)
         else:
             te = tapRes(self.xLeft, self.yLow, self.xRight, self.yUp, self.oRes, self.cross180)
             oP = getOname(self.pid, mosaicOfolder)
             cmd = cmdBase + cmdVar.format(*te, self.oRes, self.oRes, self.tempRgbName, oP)
-            print(cmd)
+            # print(cmd)
             system(cmd)
         if keppTemp:
             return
