3180988739a08d8d36ebc969515a2443f22a3f87
187345994@qq.com <catfishy_0094>
2021-01-18 18:50:44 +0800

    update

--- global_mosaic/bestSceneUI.py
+++ global_mosaic/bestSceneUI.py
@@ -3,7 +3,7 @@ class ThumbHistApp():
     def __init__(self):
         wd = tkinter.Tk()
         wd.title(u'Landsat数据挑选')
-        txt = tkinter.Entry(wd, width=10)
+        txt = tkinter.Entry(wd, width=10).pack()
         wd.mainloop()
 
 if __name__ == '__main__':
